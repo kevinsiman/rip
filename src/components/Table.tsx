3f878327102214377f451aa17b760028177d91a3
kevinsiman <kevinfernandosiman@hotmail.com>
2023-01-10 00:56:30 -0300

    change the spir

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useCallback } from "react";
 import { rip } from "./Gerador";
 
-import ReactToPrint from "react-to-print";
+import { useReactToPrint } from "react-to-print";
+
+import { Printer } from "phosphor-react";
 
 interface TableProps {
   opName: string;
@@ -15,7 +17,7 @@ interface TableProps {
 }
 
 export const Table = ({ opName, ferramenta, cota, tolMax, tolMin, toleranciaMax, toleranciaMin, description }: TableProps) => {
-  const compRef = useRef<any>();
+  const componentRef = useRef(null);
 
   let x: any[] = [];
 
@@ -28,73 +30,78 @@ export const Table = ({ opName, ferramenta, cota, tolMax, tolMin, toleranciaMax,
 
   tableGenerator();
 
+  const reactToPrintContent = useCallback(() => {
+    return componentRef.current;
+  }, [componentRef.current]);
+
+  const handlePrint = useReactToPrint({
+    content: reactToPrintContent,
+    documentTitle: "AwesomeFileName",
+  });
+
   return (
-    <div className="flex">
-      <table>
-        <tr>
-          <td
-            style={{ letterSpacing: "4px" }}
-            className="w-10 text-lg text-zinc-900 font-bold bg-zinc-300 border border-zinc-900 text-center border-r-0"
-          >
-            <div className="-rotate-90 w-11">{ferramenta}</div>
-          </td>
-          <td
-            style={{ letterSpacing: "4px" }}
-            className="w-14 text-lg text-zinc-900 font-bold bg-zinc-300 border border-zinc-900 text-center border-r-0"
-          >
-            <div className="-rotate-90 w-11 text-center">{description}</div>
-          </td>
-        </tr>
-      </table>
-      <div className="flex">
-        <table ref={compRef}>
-          <tbody>
-            {toleranciaMax.map((item: any) => (
-              <tr key={item}>
-                <td className="w-14 border border-zinc-900 text-center border-r-0">{Number(item).toFixed(3)}</td>
-              </tr>
-            ))}
-            <tr>
-              <td className="w-14 border border-zinc-900 text-center bg-zinc-300 border-r-0">{Number(cota).toFixed(3)}</td>
-            </tr>
-            {toleranciaMin.map((item: any) => (
-              <tr key={item}>
-                <td className="w-14 border border-zinc-900 text-center border-r-0">{Number(item).toFixed(3)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {/* Check Box */}
-        <table className="flex-1">
-          <tbody>
-            {toleranciaMax.map((item: any) => (
-              <tr>
-                {x.map((item, index) => (
-                  <td className="w-14 border border-zinc-900 text-center">
-                    <input name={index.toString()} type="radio" />
-                  </td>
+    <>
+      <div ref={componentRef} className="flex items-center justify-center h-full">
+        <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center">
+            <table>
+              <tbody>
+                {toleranciaMax.map((item: any) => (
+                  <tr key={item}>
+                    <td className="w-14 border border-zinc-900 text-center border-r-0">{Number(item).toFixed(3)}</td>
+                  </tr>
                 ))}
-              </tr>
-            ))}
-            <tr>
-              {x.map((item, index) => (
-                <td className="w-14 bg-zinc-300 border border-zinc-900 text-center">
-                  <input name={index.toString()} type="radio" />
-                </td>
-              ))}
-            </tr>
-            {toleranciaMin.map((item: any) => (
-              <tr>
-                {x.map((item, index) => (
-                  <td className="w-14 border border-zinc-900 text-center">
-                    <input name={index.toString()} type="radio" />
-                  </td>
+                <tr>
+                  <td className="w-14 border border-zinc-900 text-center bg-zinc-300 border-r-0">{Number(cota).toFixed(3)}</td>
+                </tr>
+                {toleranciaMin.map((item: any) => (
+                  <tr key={item}>
+                    <td className="w-14 border border-zinc-900 text-center border-r-0">{Number(item).toFixed(3)}</td>
+                  </tr>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              </tbody>
+            </table>
+            {/* Check Box */}
+            <table className="flex-1">
+              <tbody>
+                {toleranciaMax.map((item: any) => (
+                  <tr>
+                    {x.map((item, index) => (
+                      <td className="w-14 border border-zinc-900 text-center">
+                        <input name={index.toString()} type="radio" />
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+                <tr>
+                  {x.map((item, index) => (
+                    <td className="w-14 bg-zinc-300 border border-zinc-900 text-center">
+                      <input name={index.toString()} type="radio" />
+                    </td>
+                  ))}
+                </tr>
+                {toleranciaMin.map((item: any) => (
+                  <tr>
+                    {x.map((item, index) => (
+                      <td className="w-14 border border-zinc-900 text-center">
+                        <input name={index.toString()} type="radio" />
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <button
+        type="button"
+        className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+        onClick={handlePrint}
+      >
+        <Printer size={32} weight="fill" />
+      </button>
+    </>
   );
 };
