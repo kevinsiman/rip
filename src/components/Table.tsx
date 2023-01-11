4864fd4db48b93b13e484fbff0fdec2ee442efb2
kevinsiman <kevinfernandosiman@hotmail.com>
2023-01-11 00:05:19 -0300

    major c

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -53,9 +53,9 @@ export const Table = ({ item, op }: TableProps) => {
 
                 {index === 4 ? (
                   <>
-                    <div className="mb-28"></div>
+                    <div className="mb-32 py-5"></div>
                     <Header />
-                    <div className="mb-5"></div>
+                    <div className="mb-2"></div>
                     <SubHeader />
                   </>
                 ) : null}
@@ -74,13 +74,15 @@ export const Table = ({ item, op }: TableProps) => {
         </div>
       </div>
 
-      <button
-        type="button"
-        className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-        onClick={handlePrint}
-      >
-        <Printer size={32} weight="fill" />
-      </button>
+      <div className="flex justify-center">
+        <button
+          type="button"
+          className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          onClick={handlePrint}
+        >
+          <Printer size={32} weight="fill" />
+        </button>
+      </div>
     </>
   );
 };
